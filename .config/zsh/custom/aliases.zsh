007f967b1c2ab0887cdfca81930edbf4caefaea7
Manuel Cattelan <cattelanmanuel07@gmail.com>
2023-12-01 17:01:37 +0100

    feat(zsh): add fzf functions

--- .config/zsh/custom/aliases.zsh
+++ .config/zsh/custom/aliases.zsh
@@ -1,8 +1,12 @@
 # Override built-ins with better defaults.
-alias vi=nvim
-alias vim=nvim
+alias vi='nvim'
+alias vim='nvim'
 
 alias ls='ls -G'
 alias la='ls -a'
 alias ll='ls -l'
 alias lal='ls -al'
+
+# Function aliases for easier execution.
+alias pf='fuzzyfile'
+alias pd='fuzzydir'
---
+++ .config/zsh/plugins/config/fzf.zsh
@@ -0,0 +1,5 @@
+# https://github.com/junegunn/fzf
+source "/opt/homebrew/opt/fzf/shell/completion.zsh"
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+export FZF_DEFAULT_OPTS="--height 50% --reverse --bind change:top"
