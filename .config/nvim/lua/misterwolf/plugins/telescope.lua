fbb06906055cbede4ef697dcc672381c794eaaf1
Manuel Cattelan <cattelanmanuel07@gmail.com>
2023-12-02 15:04:44 +0100

    feat(nvim): improve telescope behaviour

--- .config/nvim/lua/misterwolf/plugins/telescope.lua
+++ .config/nvim/lua/misterwolf/plugins/telescope.lua
@@ -6,30 +6,83 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   keys = {
-    { "<leader>pf", ":lua require'telescope.builtin'.find_files()<CR>", desc = "Project Find (cwd)" },
-    { "<leader>pF", ":lua require'telescope.builtin'.git_files()<CR>", desc = "Project Find (gwd)" },
-    { "<leader>pg", ":lua require'telescope.builtin'.live_grep()<CR>", desc = "Project Grep (cwd)" },
-  },
-  opts = {
-    defaults = {
-      mappings = {
-        i = {
-          ["<C-i>"] = function(...)
-            return require("telescope.actions.layout").toggle_preview(...)
-          end,
-        },
-      },
-      layout_config = {
-        preview_width = 80,
-      },
-      borderchars = {
-        { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-        prompt = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-        results = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-        preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-      },
+    {
+      "<leader>pf",
+      function()
+        require("telescope.builtin").find_files({ hidden = true })
+      end,
+      desc = "Project Find (cwd)",
+    },
+    {
+      "<leader>pF",
+      function()
+        require("telescope.builtin").git_files({ show_untracked = true })
+      end,
+      desc = "Project Find (gwd)",
+    },
+    {
+      "<leader>pg",
+      function()
+        require("telescope.builtin").live_grep()
+      end,
+      desc = "Project Grep (cwd)",
+    },
+    {
+      "<leader>pG",
+      function()
+        local function is_inside_git_project()
+          vim.fn.system("git rev-parse --is-inside-work-tree")
+          return vim.v.shell_error == 0
+        end
+
+        local function get_git_project_root()
+          local git_project_root = vim.fn.finddir(".git", ".;")
+          return vim.fn.fnamemodify(git_project_root, ":h")
+        end
+
+        local opts = {}
+        if is_inside_git_project() then
+          opts = { cwd = get_git_project_root() }
+        end
+
+        require("telescope.builtin").live_grep(opts)
+      end,
+      desc = "Project Grep (gwd with fallback)",
     },
   },
+  opts = function()
+    local telescopeConfig = require("telescope.config")
+
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+    local file_ignore_patterns = { ".git/", "node_modules/*" }
+
+    table.insert(vimgrep_arguments, "--hidden")
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
+
+    return {
+      defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+        file_ignore_patterns = file_ignore_patterns,
+        mappings = {
+          i = {
+            ["<C-i>"] = function(...)
+              return require("telescope.actions.layout").toggle_preview(...)
+            end,
+          },
+        },
+        layout_config = {
+          preview_width = 80,
+        },
+        borderchars = {
+          { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+          prompt = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+          results = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+          preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+        },
+      },
+    }
+  end,
   config = function(_, opts)
     local telescope = require("telescope")
 
